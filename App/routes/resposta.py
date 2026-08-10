986c7c2b231d9c2a6563a1477b350ab9d1deeb23
Kael <ms6334744@gmail.com>
2026-08-10 12:45:48 -0300

    junção do back com front

---
+++ App/routes/resposta.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify
+from database import conectar
+from flask_jwt_extended import jwt_required
+
+resposta_bp = Blueprint('resposta', __name__)
+
+@resposta_bp.route('/respostas', methods=['GET'])
+@jwt_required()
+def listar_respostas():
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM resposta")
+    respostas = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(respostas)
