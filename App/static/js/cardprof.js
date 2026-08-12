a8bc11fde9b941e3e36b0e4137b2d6d0d07a2e7d
Kael <ms6334744@gmail.com>
2026-08-12 01:08:34 -0300

    deletado antigo

--- App/static/js/cardprof.js
+++
@@ -1,9 +0,0 @@
-function entrarSala() {
-            alert('Entrando na sala 3º TDS A - Biologia');
-        }
-        function verHorario() {
-            alert('Abrindo horário');
-        }
-        function verAtividades() {
-            alert('Abrindo atividades');
-        }
