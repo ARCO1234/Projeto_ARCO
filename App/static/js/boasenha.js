f9ca79a8235b5f27a10e14a2eac20ed2279584d2
Kael <ms6334744@gmail.com>
2026-08-12 13:41:21 -0300

    removido antigo

--- App/static/js/boasenha.js
+++
@@ -1,7 +0,0 @@
-const btnVoltar = document.getElementById("btnVoltar");
-
-btnVoltar.addEventListener("click", function () {
-    window.location.href = "/login-page";
-});
-
-console.log("Tela de sucesso carregada com sucesso.");
